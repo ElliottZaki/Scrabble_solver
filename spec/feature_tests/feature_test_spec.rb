374dbfdf04bc9fa1391ececf8e4b1e64520d810a
Elliott Zaki <elliottzaki@Elliotts-Air.broadband>
2022-06-17 20:13:47 +0100

    Refractored tests, updates features tests.rb and amended README.MD

--- spec/feature_tests/feature_test_spec.rb
+++ spec/feature_tests/feature_test_spec.rb
@@ -1,4 +1,4 @@
 require './lib/scrabble_solver.rb'
-scrabble_solver = Scrabble_solver.new("ruby")
-# scrabble_solver.split_string(scrabble_solver.word)
+scrabble = Scrabble_solver.new("ruby")
 SCORES = {"A"=>1, "E"=>1, "I"=>1, "O"=>1, "U"=>1, "L"=>1, "N"=>1, "R"=>1, "S"=>1, "T"=>1, "D"=>2, "G"=>2, "B"=>3, "C"=>3, "M"=>3, "P"=>3, "F"=>4, "H"=>4, "V"=>4, "W"=>4, "Y"=>4, "K"=>5, "J"=>8, "X"=>8, "Q"=>10, "Z"=>10}
+scrabble.tally_score(SCORES)
--- spec/scrabble_solver_spec.rb
+++ spec/scrabble_solver_spec.rb
@@ -1,16 +1,19 @@
 require './lib/scrabble_solver.rb'
 
 describe Scrabble_solver do
-  it 'creates a class that assigns a word & upcases it as an attribute "@word" upon initialization' do
-    scrabble_solver = Scrabble_solver.new("ruby")
-    expect(scrabble_solver.word).to eq(["R", "U", "B", "Y"])
+
+  before do
+    @scrabble = Scrabble_solver.new("ruby")
+    @SCORES = {"A"=>1, "E"=>1, "I"=>1, "O"=>1, "U"=>1, "L"=>1, "N"=>1, "R"=>1, "S"=>1, "T"=>1, "D"=>2, "G"=>2, "B"=>3, "C"=>3, "M"=>3, "P"=>3, "F"=>4, "H"=>4, "V"=>4, "W"=>4, "Y"=>4, "K"=>5, "J"=>8, "X"=>8, "Q"=>10, "Z"=>10}
+  end 
+  
+  it 'creates a class that assigns a word, splits & upcases it as an attribute' do
+    expect(@scrabble.word).to eq(["R", "U", "B", "Y"])
   end
 
   describe '#tally_score' do
     it 'tallies the score depending on the letters input' do
-      SCORES = {"A"=>1, "E"=>1, "I"=>1, "O"=>1, "U"=>1, "L"=>1, "N"=>1, "R"=>1, "S"=>1, "T"=>1, "D"=>2, "G"=>2, "B"=>3, "C"=>3, "M"=>3, "P"=>3, "F"=>4, "H"=>4, "V"=>4, "W"=>4, "Y"=>4, "K"=>5, "J"=>8, "X"=>8, "Q"=>10, "Z"=>10}
-      scrabble_solver = Scrabble_solver.new("ruby")
-      expect(scrabble_solver.tally_score(SCORES)).to eq(9)
+      expect(@scrabble.tally_score(@SCORES)).to eq(9)
     end
   end
 
