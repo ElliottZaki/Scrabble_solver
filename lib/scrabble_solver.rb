0f86da29c8ef8d64e80edc3730bf904b97de27a3
Elliott Zaki <elliottzaki@Elliotts-Air.broadband>
2022-06-17 19:31:04 +0100

    Exercise completed. Desired outcome achieved. Tests pass. Refractoring pending.

--- lib/scrabble_solver.rb
+++ lib/scrabble_solver.rb
@@ -1,25 +1,23 @@
 class Scrabble_solver
 
-attr_reader :word
+  attr_reader :word
 
-def initialize(word)
-  @word = word
-end
+  def initialize(word)
+    @word = word.upcase.split('')
+  end
 
-scores = Hash.new
+SCORES = Hash.new
+["A", "E", "I", "O", "U", "L", "N", "R", "S", "T" ].each {|letter| SCORES[letter] = 1}
+["D", "G"].each {|letter| SCORES[letter] = 2}
+["B", "C", "M", "P"].each {|letter| SCORES[letter] = 3}
+["F", "H", "V", "W", "Y"].each {|letter| SCORES[letter] = 4}
+["K"].each {|letter| SCORES[letter] = 5}
+["J", "X"].each {|letter| SCORES[letter] = 8}
+["Q", "Z"].each {|letter| SCORES[letter] = 10}
 
-["A", "E", "I", "O", "U", "L", "N", "R", "S", "T" ].each {|letter| scores[letter] = 1}
-["D", "G"].each {|letter| scores[letter] = 2}
-["B", "C", "M", "P"].each {|letter| scores[letter] = 3}
-["F", "H", "V", "W", "Y"].each {|letter| scores[letter] = 4}
-["K"].each {|letter| scores[letter] = 5}
-["J", "X"].each {|letter| scores[letter] = 8}
-["Q", "Z"].each {|letter| scores[letter] = 10}
-
-def split_string(word)
-  individual_letters = @word.split('')
-end
-
-
-
+  def tally_score(scores)
+    total = 0
+    word.each {|letter| total += scores[letter]}
+      return total
+  end
 end
--- spec/scrabble_solver_spec.rb
+++ spec/scrabble_solver_spec.rb
@@ -1,17 +1,16 @@
 require './lib/scrabble_solver.rb'
 
 describe Scrabble_solver do
-
-  it 'creates a class that assigns a word as an attribute "@word" upon initialization' do
+  it 'creates a class that assigns a word & upcases it as an attribute "@word" upon initialization' do
     scrabble_solver = Scrabble_solver.new("ruby")
-    expect(scrabble_solver.word).to eq("ruby")
+    expect(scrabble_solver.word).to eq(["R", "U", "B", "Y"])
   end
 
-  describe '#split_string' do
-    it 'splits the string into individual letters' do
+  describe '#tally_score' do
+    it 'tallies the score depending on the letters input' do
+      SCORES = {"A"=>1, "E"=>1, "I"=>1, "O"=>1, "U"=>1, "L"=>1, "N"=>1, "R"=>1, "S"=>1, "T"=>1, "D"=>2, "G"=>2, "B"=>3, "C"=>3, "M"=>3, "P"=>3, "F"=>4, "H"=>4, "V"=>4, "W"=>4, "Y"=>4, "K"=>5, "J"=>8, "X"=>8, "Q"=>10, "Z"=>10}
       scrabble_solver = Scrabble_solver.new("ruby")
-      individual_letters = scrabble_solver.split_string(scrabble_solver.word)
-      expect(individual_letters).to eq(["r", "u", "b", "y"])
+      expect(scrabble_solver.tally_score(SCORES)).to eq(9)
     end
   end
 
